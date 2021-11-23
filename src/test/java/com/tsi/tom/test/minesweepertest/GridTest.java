149d0fee7c34b460ba6d12027a5dd4317538930e
Thomas Crosby <thomascrosby3@gmail.com>
2021-11-23 16:23:45 +0000

    implemented seeded tests

--- src/test/java/com/tsi/tom/test/minesweepertest/GridTest.java
+++ src/test/java/com/tsi/tom/test/minesweepertest/GridTest.java
@@ -19,8 +19,7 @@ public class GridTest {
         assertEquals(testGrid.getgSize(), (20 * 20), "Grid size is based on values entered");
         assertEquals(testGrid.getgTotalMines(), (20 * 20) / 8, "Mines are total size / 8");
         assertEquals(testGrid.toString() instanceof String, true, "the board is displayed as a string");
-
-
-
+        assertEquals(testGrid.selectTile(0, 0), 3, "seeded so should always be a clear");
+        assertEquals(testGrid.checkSurroundingTiles(0, 0), 0, "seeded so should always not have any bombs around it");
     }
 }
